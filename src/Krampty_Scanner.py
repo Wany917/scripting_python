ac42eed7db085e243c9d529d6f921a26f4e3623b
{{Yvann}} <91730578+Wany917@users.noreply.github.com>
2023-12-14 18:18:03 +0100

    Feature/read file (#6)
    * list function added :sparkles:
    
    * read function done :sparkle:
    
    * Update main.yml
    
    * node modules added
    
    * eslin

--- src/Krampty_Scanner.py
+++ src/Krampty_Scanner.py
@@ -3,3 +3,9 @@ import ipaddress,os, platform, subprocess, argparse,socket,sys
 
 def list_files_in_dir(directory):
     return os.listdir(directory)
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+      
+    return os.listdir(directory)
