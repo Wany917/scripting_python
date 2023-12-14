4b9989988c3804750c2f6c22873ded15b2bb927f
{{Yvann}} <91730578+Wany917@users.noreply.github.com>
2023-12-14 23:03:12 +0100

    MEP #2
    * added linter
    
    * push
    
    * list function added :sparkles:
    
    * Feature/read file (#6)
    
    * list function added :sparkles:
    
    * read function done :sparkle:
    
    * Update main.yml
    
    * node modules added
    
    * eslin
    
    * -S
    
    Display Interfaces
    
    * network int retrieval done
    
    * parse network info done
    
    * retrieve ip config done
    
    * write interface details done
    
    * argument-parsing done
    
    * retrieve subnet mask done
    
    * ping network done
    
    * socket scan done
    
    * main function done
    
    ---------
    
    Co-authored-by: Quaume <comaloisi@gmail.com>
    Co-authored-by: Strandiing <ssstranding@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,4 +9,8 @@ let retrieve_ip_config = "done"
 
 let write_interface_details = "done"
 
+let main_function = "done"
+let ping_network = "done"
+let retrieve_subnet_mask = "done"
+let socket_scan = "done"
 console.log(aa)
